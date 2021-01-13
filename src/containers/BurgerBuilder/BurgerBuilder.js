bd82d0ea58f23c3b4c30abaa646a822934e8ba76
jthanawat <kla13thanawat@gmail.com>
2021-01-13 20:38:02 +0700

    updated

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,7 +31,7 @@ class BurgerBuilder extends Component {
 
   componentDidMount() {
     axios
-      .get("https://react-my-burger-1d52d.firebaseio.com/ingredients")
+      .get("https://react-my-burger-1d52d.firebaseio.com/ingredients.json")
       .then((response) => {
         this.setState({ ingredients: response.data });
       })
